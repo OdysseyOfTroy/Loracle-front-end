7ef95e35bfb71df0194900ade056988411daa435
Troy <troy-matthews@hotmail.com>
2020-12-08 04:21:49 +0000

    created base navbar

---
+++ src/Components/Navbar/MenuItems.js
@@ -0,0 +1,19 @@
+export const MenuItems = 
+[
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Sign In',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Sign Up',
+        url: '#',
+        cName: 'nav-links-mobile'
+    }
+
+]
