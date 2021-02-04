d8c944fcefca976c70769f71eb270a7d3c87c9ba
Troy <troy-matthews@hotmail.com>
2021-02-04 22:11:39 +0000

    created two alternate main page navigation templates

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import MainPage from "./components/MainPage";
+import MainPageAlt from "./components/MainPageAlt"
 
 function App() {
   return (
     <Router>
-      <Route path="/" exact component={MainPage}/>
+      <Route path="/" exact component={MainPageAlt}/>
     </Router>
   );
 }
--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,14 +1,21 @@
 import React from "react";
 import Sidebar from "./Sidebar.component";
+import "../css/Mainpage.css"
 
 function MainPage() {
     return (
     <div className="Nav">
         <Sidebar width={200} height={"100vh"}>
-            <p>Nav item</p>
-            <p>Nav item</p>
-            <p>Nav item</p>
-            <p>Nav item</p>
+            <div className="category"> group 
+                <p className="Identifier">Nav item</p>
+                <p classname="Identifier">Nav item</p>
+            </div>
+
+            <div className="category"> group
+                <p classname="Identifier">Nav item</p>
+                <p classname="Identifier">Nav item</p>
+                <p classname="Identifier">Nav item</p>
+            </div>
         </Sidebar>
     </div>
        );
