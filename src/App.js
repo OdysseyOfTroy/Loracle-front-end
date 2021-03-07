d9cf0722ad2ef6a6677876c1cae96e5073e97daa
troy <mrtroymatthews@gmail.com>
2021-03-07 05:57:15 +0000

    added API get category connection.

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import MainPage from "./components/MainPage";
 import MainPageAlt from "./components/MainPageAlt"
 import ContainersList from './components/ContainerList'
+import CategoryList from './components/CategoryList'
 
 function App() {
   return (
     <Router>
-      <Route path="/" exact component={ContainersList}/>
+      <Route path="/" exact component={CategoryList}/>
     </Router>
   );
 }
