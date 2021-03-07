8ef023461a54beda38f4f954d7f0335613c329f0
troy <mrtroymatthews@gmail.com>
2021-03-07 06:12:11 +0000

    added API get identifiers connection.

--- src/App.js
+++ src/App.js
@@ -6,11 +6,12 @@ import MainPage from "./components/MainPage";
 import MainPageAlt from "./components/MainPageAlt"
 import ContainersList from './components/ContainerList'
 import CategoryList from './components/CategoryList'
+import IdentifierList from './components/IdentifierList'
 
 function App() {
   return (
     <Router>
-      <Route path="/" exact component={CategoryList}/>
+      <Route path="/" exact component={IdentifierList}/>
     </Router>
   );
 }
