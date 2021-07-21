69abec5d65944d1f03e6e568d3408a7aa8b9ee3b
troy <mrtroymatthews@gmail.com>
2021-07-21 13:43:06 +0100

    created user login page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 import MainPage from "./components/MainPage";
-import MainPageAlt from "./components/MainPageAlt"
+import MainPageAlt from "./components/MainPageAlt";
+import Login from './components/LogIn';
 
 function App() {
   return (
     <Router>
-      <Route path="/" exact component={MainPageAlt}/>
+      <Route path="/" exact component={Login}/>
     </Router>
   );
 }
