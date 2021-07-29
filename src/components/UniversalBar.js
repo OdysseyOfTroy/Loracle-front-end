0eeabbb080395d2bf168ed3f8bd9a9ed841e40ef
troy <mrtroymatthews@gmail.com>
2021-07-29 15:55:04 +0100

    created universalbar with sign out functionality

---
+++ src/components/UniversalBar.js
@@ -0,0 +1,22 @@
+import React from "react";
+import AuthenticationService from "./Connections/Authentication.service";
+import { Navbar } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+
+function Universalbar() {
+
+    const logout = () => {
+        AuthenticationService.logout();
+        window.location.replace("/");
+    }
+
+    return (
+        <Navbar className="bg-primary w-100 d-inline-flex justify-content-between p-0 mb-lg-2">
+            <Button onClick={logout}>
+                log out
+            </Button>
+        </Navbar>
+    )
+}
+
+export default Universalbar;
