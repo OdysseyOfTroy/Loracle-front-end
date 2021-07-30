920c1467acf7c3b02ebf0a56097138b38c33ddd3
troy <mrtroymatthews@gmail.com>
2021-07-30 17:47:47 +0100

    added view and delete buttons to containers

--- src/components/Dashboard/Container.List.js
+++ src/components/Dashboard/Container.List.js
@@ -4,15 +4,19 @@ import "../../css/Dashboard.css";
 function ContainerList(props) {
     
     return (
-        <button className="card">
+        <div className="card">
         <div className="custom-card">
         
           <div className="card-body">
             <h5 className="card-title">{props.title}</h5>
             <p className="card-text">{props.description}</p>
           </div>
+        <div className="button-Actions">
+          <button className="button-function delete">Delete</button>
+          <button className="button-function view">View</button>
+        </div>
       </div>
-      </button>
+      </div>
     )
 }
 
